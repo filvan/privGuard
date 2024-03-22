baf13e242ba0afef2dbe45006433dacff19ec62a
filvan <fil.vannutelli@stud.uniroma3.it>
2024-03-22 17:33:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 #python3.6 -m venv venv
 #source venv/bin/activate
-#pip install -r requirements.txt
-export PRIVGUARD=$(pwd)
+pip install -r requirements.txt
+echo "export PYTHONPATH=\"${PYTHONPATH}:${PWD}\"" >> ~/.bash_profile
+echo "export PRIVGUARD=\"${PWD}\"" >> ~/.bash_profile
+source ~/.bash_profile
+echo "Setup complete"
