dc78311d5e41653416a8d6ebcdf19b6f4932e3aa
filvan <fil.vannutelli@stud.uniroma3.it>
2024-03-22 17:48:54 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #python3.6 -m venv venv
 #source venv/bin/activate
 pip install -r requirements.txt
-sudo sh -c "echo 'export PYTHONPATH=\"${PYTHONPATH}:${PWD}\"' >> ~/.bash_profile"
-sudo sh -c "echo 'export PRIVGUARD=\"${PWD}\"' >> ~/.bash_profile"
+echo "export PYTHONPATH=\"${PYTHONPATH}:${PWD}\"" | sudo tee -a ~/.bash_profile > /dev/null
+echo "export PRIVGUARD=\"${PWD}\"" | sudo tee -a ~/.bash_profile > /dev/null
 source ~/.bash_profile
 echo "Setup complete"
