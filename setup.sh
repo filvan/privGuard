f9c4c1c650d2bbfe094d4b788a183245f6b726c9
filvan <fil.vannutelli@stud.uniroma3.it>
2024-03-22 18:26:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 echo "export PYTHONPATH=\"${PYTHONPATH}:${PWD}\"" | sudo tee -a ~/.zprofile > /dev/null
 echo "export PRIVGUARD=\"${PWD}\"" | sudo tee -a ~/.zprofile > /dev/null
 echo "Setup complete!"
-echo "I am going to exit in a few seconds"
+echo "I am going to exit in a few seconds."
 echo "If your env vars are not set yet, please run 'source ~/.zprofile' to apply the changes."
 sleep 5
 exit
