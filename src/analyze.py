fdeeb4e303675ae79c1af33b61fbb5c76d783ad4
filvan <fil.vannutelli@stud.uniroma3.it>
2024-06-17 11:24:00 +0200

    Update analyze.py
    Format fixes

--- src/analyze.py
+++ src/analyze.py
@@ -22,6 +22,20 @@
 
 """ PrivAnalyzer. """
 
+from importlib.util import spec_from_file_location, module_from_spec
+from src.stub_libraries import stub_pandas, stub_numpy, stub_lightgbm, stub_xgboost
+from src.stub_libraries.stub_numpy import random as stub_random
+from src.stub_libraries.stub_statsmodels.tsa.arima import model as stub_arima
+from src.stub_libraries.stub_sklearn import cross_validation as stub_cross_validation
+from src.stub_libraries.stub_sklearn import metrics as stub_metrics
+from src.stub_libraries.stub_sklearn import model_selection as stub_model_selection
+from examples.program.Libretaxi.locales import english, es, pt_br, pt_pt, ru
+# from shutil import copyfile
+# from src.parser.attribute import Satisfied
+# from src.parser.policy_tree import Policy
+import argparse
+# import json
+# import stub_numpy
 import os
 import sys
 # print(os.getcwd())
@@ -29,22 +43,7 @@ import sys
 sys.path.append(os.path.join(os.environ.get('PRIVGUARD'), 'src/parser'))
 sys.path.append(os.path.join(os.environ.get('PRIVGUARD'), "src/stub_libraries"))
 
-from importlib.util import spec_from_file_location, module_from_spec
-import json
-from shutil import copyfile
-import argparse
 
-from src.parser.attribute import Satisfied
-from src.parser.policy_tree import Policy
-
-from src.stub_libraries import stub_pandas,stub_numpy,stub_lightgbm,stub_xgboost
-#import stub_numpy
-from src.stub_libraries.stub_numpy import random as stub_random
-from src.stub_libraries.stub_statsmodels.tsa.arima import model as stub_arima
-from src.stub_libraries.stub_sklearn import cross_validation as stub_cross_validation
-from src.stub_libraries.stub_sklearn import metrics as stub_metrics
-from src.stub_libraries.stub_sklearn import model_selection as stub_model_selection
-from examples.program.Libretaxi.locales import english, es, fa, pt_br, pt_pt, ru
 program_map = {
     0: "./examples/program/ehr_example.py",
     # 1: "./examples/program/1_fraud_detection.py",
@@ -72,12 +71,11 @@ program_map = {
     23: "./examples/program/23_web_traffic_forecasting.py",
     24: "./examples/program/cpra_example.py",
 
-
     25: "./examples/program/Libretaxi/libretaxi_example.py",
     26: "./examples/program/Libretaxi/repository/libretaxi_saveUser.py",
-    #27: "./examples/program/Libretaxi/libretaxi_ModifyUser.py",
+    # 27: "./examples/program/Libretaxi/libretaxi_ModifyUser.py",
     28: "./examples/program/Libretaxi/libretaxi_callback.py",
-    #29: "./examples/program/Libretaxi/libretaxi_validation.py",
+    # 29: "./examples/program/Libretaxi/libretaxi_validation.py",
     30: "./examples/program/Libretaxi/repository/libretaxi_savePost.py",
     31: "./examples/program/Libretaxi/repository/libretaxi_findUserAround.py",
     32: "./examples/program/Libretaxi/repository/libretaxi_recentPosts.py",
@@ -89,7 +87,7 @@ program_map = {
     38: "./examples/program/Libretaxi/repository/libretaxi_findPost.py",
     39: "./examples/program/Libretaxi/repository/libretaxi_findUser.py",
     40: "./examples/program/Libretaxi/libretaxi_locale.py",
-    #selfmailbot
+    # selfmailbot
     50: "./examples/program/selfmailbot/app/confirm_email.py",
     51: "./examples/program/selfmailbot/app/resend.py",
     52: "./examples/program/selfmailbot/app/reset_email.py",
@@ -106,7 +104,7 @@ program_map = {
     62: "./examples/program/selfmailbot/models/get_user_by_confirmation_link.py",
     63: "./examples/program/selfmailbot/models/get_user_instance.py",
 
-    #traccar
+    # traccar
 
     70: "./examples/program/traccar/analyze/columns/get_all_columns_analizer.py",
     71: "./examples/program/traccar/analyze/columns/exclude_columns.py",
@@ -148,9 +146,9 @@ data_map = {
     24: "./examples/data/cpra/",
     25: "./examples/data/libretaxi_cpra/",
     26: "./examples/data/libretaxi_cpra/",
-    #27: "./examples/data/libretaxi_cpra/",
+    # 27: "./examples/data/libretaxi_cpra/",
     28: "./examples/data/libretaxi_cpra/",
-    #29: "./examples/data/libretaxi_cpra/",
+    # 29: "./examples/data/libretaxi_cpra/",
     30: "./examples/data/libretaxi_cpra/",
     31: "./examples/data/libretaxi_cpra/",
     32: "./examples/data/libretaxi_cpra/",
@@ -180,7 +178,7 @@ data_map = {
     63: "./examples/data/selfmailbot/",
     64: "./examples/data/selfmailbot/",
 
-    #traccar
+    # traccar
     70: "./examples/data/traccar/",
     71: "./examples/data/traccar/",
     72: "./examples/data/traccar/",
@@ -194,20 +192,23 @@ data_map = {
 }
 
 lib_map = {
-    0: {'numpy':stub_numpy, 'pandas':stub_pandas},
-    4: {'cross_validation': stub_cross_validation, 'metrics': stub_metrics, 'numpy': stub_numpy, 'pandas': stub_pandas, 'xgboost': stub_xgboost},
-    5: {'lgb':stub_lightgbm, 'metrics': stub_metrics, 'model_selection':stub_model_selection, 'numpy':stub_numpy, 'pandas':stub_pandas, 'random':stub_random},
-    6: {'numpy':stub_numpy, 'pandas':stub_pandas},
+    0: {'numpy': stub_numpy, 'pandas': stub_pandas},
+    4: {'cross_validation': stub_cross_validation, 'metrics': stub_metrics, 'numpy': stub_numpy, 'pandas': stub_pandas,
+        'xgboost': stub_xgboost},
+    5: {'lgb': stub_lightgbm, 'metrics': stub_metrics, 'model_selection': stub_model_selection, 'numpy': stub_numpy,
+        'pandas': stub_pandas, 'random': stub_random},
+    6: {'numpy': stub_numpy, 'pandas': stub_pandas},
 
-    7: {'numpy': stub_numpy, 'pandas': stub_pandas,'lgb':stub_lightgbm, 'metrics': stub_metrics, 'model_selection':stub_model_selection},
+    7: {'numpy': stub_numpy, 'pandas': stub_pandas, 'lgb': stub_lightgbm, 'metrics': stub_metrics,
+        'model_selection': stub_model_selection},
 
-    23: {'numpy':stub_numpy, 'pandas':stub_pandas, 'arima': stub_arima},
-    24: {'numpy':stub_numpy, 'pandas':stub_pandas},
+    23: {'numpy': stub_numpy, 'pandas': stub_pandas, 'arima': stub_arima},
+    24: {'numpy': stub_numpy, 'pandas': stub_pandas},
     25: {'numpy': stub_numpy, 'pandas': stub_pandas},
     26: {'numpy': stub_numpy, 'pandas': stub_pandas},
     # 27: {'numpy': stub_numpy, 'pandas': stub_pandas},
     28: {'numpy': stub_numpy, 'pandas': stub_pandas},
-    #29: {'numpy': stub_numpy, 'pandas': stub_pandas},
+    # 29: {'numpy': stub_numpy, 'pandas': stub_pandas},
     30: {'numpy': stub_numpy, 'pandas': stub_pandas},
     31: {'numpy': stub_numpy, 'pandas': stub_pandas},
     32: {'numpy': stub_numpy, 'pandas': stub_pandas},
@@ -220,7 +221,7 @@ lib_map = {
     39: {'numpy': stub_numpy, 'pandas': stub_pandas},
     40: {'numpy': stub_numpy, 'pandas': stub_pandas},
     41: {'numpy': stub_numpy, 'pandas': stub_pandas},
-    42: {'numpy': stub_numpy, 'pandas': stub_pandas,'cross_validation': stub_cross_validation},
+    42: {'numpy': stub_numpy, 'pandas': stub_pandas, 'cross_validation': stub_cross_validation},
     50: {'pandas': stub_pandas},
     51: {'pandas': stub_pandas},
     52: {'pandas': stub_pandas},
@@ -253,11 +254,14 @@ locales = {
     'english': english,
     'spanish': es,
     'portugese': pt_pt,
-    'portugese_brasil':pt_br,
+    'portugese_brasil': pt_br,
     'russian': ru
 }
+
+
 def analyze(module, data_folder, lib_list):
     return module.run(data_folder, lightgbm=stub_lightgbm, **lib_list)
+
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -265,44 +269,45 @@ def parse():
     parser.add_argument('--post_report', help='Post action', type=str, default='REPORT_POST')
     parser.add_argument('--lat', help="Latitude for user", type=str, default="")
     parser.add_argument('--lon', help="Longitude for user", type=str, default="")
-    parser.add_argument('--user_id', help='User to find', type = str, default='n_consumer')
-    parser.add_argument('--consent_use', help='Consent use data', type = str, default='N')
-    parser.add_argument('--consent_share', help='Consent share data', type = str, default='N')
-    parser.add_argument('--consent_sell', help='Consent sell data', type = str, default='N')
-    parser.add_argument('--consent_collection', help='Consent collection data', type = str, default='N')
-    parser.add_argument('--consent_retention', help='Consent retention data', type = str, default='N')
-    parser.add_argument('--guardian_consent', help='Consent from guardians data', type = str, default='N')
-    parser.add_argument('--request_deletion', help='Request deletion of data from user', type = str, default='N')
-    parser.add_argument('--request_disclosure', help='Request disclosure of data from user', type = str, default='N')
-    parser.add_argument('--request_inaccurate', help='Request modification of data from user because inaccurate', type = str, default='N')
-    parser.add_argument('--limit_use', help='User request limit use', type = str, default='N')
-    parser.add_argument('--age', help='User age', type = int, default=16)
-    parser.add_argument('--menu_id', help='Menu ID associated to user', type = str, default="")
-    parser.add_argument('--username', help='User username', type = str, default="")
-    parser.add_argument('--first_name', help='User first name', type = str, default="")
-    parser.add_argument('--last_name', help='User last name', type = str, default="")
-    parser.add_argument('--language_code', help='User language code', type = str, default="")
-    parser.add_argument('--report_cnt', help='User report count', type = int, default=0)
-    parser.add_argument('--shadow_banned', help='User is shadow banned', type = str, default="N")
-    parser.add_argument('--text', help='User text message', type = str, default="")
-    parser.add_argument('--post_id', help='Id of post', type = str, default="")
-    parser.add_argument('--locales', help = 'Language code', type = str, default='english')
+    parser.add_argument('--user_id', help='User to find', type=str, default='n_consumer')
+    parser.add_argument('--consent_use', help='Consent use data', type=str, default='N')
+    parser.add_argument('--consent_share', help='Consent share data', type=str, default='N')
+    parser.add_argument('--consent_sell', help='Consent sell data', type=str, default='N')
+    parser.add_argument('--consent_collection', help='Consent collection data', type=str, default='N')
+    parser.add_argument('--consent_retention', help='Consent retention data', type=str, default='N')
+    parser.add_argument('--guardian_consent', help='Consent from guardians data', type=str, default='N')
+    parser.add_argument('--request_deletion', help='Request deletion of data from user', type=str, default='N')
+    parser.add_argument('--request_disclosure', help='Request disclosure of data from user', type=str, default='N')
+    parser.add_argument('--request_inaccurate', help='Request modification of data from user because inaccurate',
+                        type=str, default='N')
+    parser.add_argument('--limit_use', help='User request limit use', type=str, default='N')
+    parser.add_argument('--age', help='User age', type=int, default=16)
+    parser.add_argument('--menu_id', help='Menu ID associated to user', type=str, default="")
+    parser.add_argument('--username', help='User username', type=str, default="")
+    parser.add_argument('--first_name', help='User first name', type=str, default="")
+    parser.add_argument('--last_name', help='User last name', type=str, default="")
+    parser.add_argument('--language_code', help='User language code', type=str, default="")
+    parser.add_argument('--report_cnt', help='User report count', type=int, default=0)
+    parser.add_argument('--shadow_banned', help='User is shadow banned', type=str, default="N")
+    parser.add_argument('--text', help='User text message', type=str, default="")
+    parser.add_argument('--post_id', help='Id of post', type=str, default="")
+    parser.add_argument('--locales', help='Language code', type=str, default='english')
 
-    parser.add_argument('--attachment', help = 'Attachment email', type = int, default=None)
-    parser.add_argument('--attachment_name', help = 'Attachment name for email', type = str, default='')
-    parser.add_argument('--email', help = 'email address', type = str, default='')
-    parser.add_argument('--subject', help = 'Subject for email', type = str, default='')
-    parser.add_argument('--link', help = 'Confirmation link', type = str, default='')
-    parser.add_argument('--key',help ='Confirmation key for email', type = str, default = '')
+    parser.add_argument('--attachment', help='Attachment email', type=int, default=None)
+    parser.add_argument('--attachment_name', help='Attachment name for email', type=str, default='')
+    parser.add_argument('--email', help='email address', type=str, default='')
+    parser.add_argument('--subject', help='Subject for email', type=str, default='')
+    parser.add_argument('--link', help='Confirmation link', type=str, default='')
+    parser.add_argument('--key', help='Confirmation key for email', type=str, default='')
 
-    parser.add_argument('--clazz',help='class to access', type = str, default='Device' )
-    parser.add_argument('--columns', nargs='*', type=str,default=[])
-    parser.add_argument('--deviceid',help='Identifier device to find', type=str,default='n_device')
-    parser.add_argument('--new_val', help='New value for setter', type = str, default = '')
+    parser.add_argument('--clazz', help='class to access', type=str, default='Device')
+    parser.add_argument('--columns', nargs='*', type=str, default=[])
+    parser.add_argument('--deviceid', help='Identifier device to find', type=str, default='n_device')
+    parser.add_argument('--new_val', help='New value for setter', type=str, default='')
     args = parser.parse_args()
-    user = {'ConsumerID':args.user_id,
-            'ConsentUse':args.consent_use,
-            'ConsentShare':args.consent_share,
+    user = {'ConsumerID': args.user_id,
+            'ConsentUse': args.consent_use,
+            'ConsentShare': args.consent_share,
             'ConsentSell': args.consent_sell,
             'ConsentCollection': args.consent_collection,
             'ConsentRetention': args.consent_retention,
@@ -317,18 +322,18 @@ def parse():
             'LastName': args.last_name,
             'Longitude': args.lon,
             'Latitude': args.lat,
-            'LanguageCode':args.language_code,
-            'ReportCnt':args.report_cnt,
+            'LanguageCode': args.language_code,
+            'ReportCnt': args.report_cnt,
             'ShadowBanned': args.shadow_banned
             }
 
     extra_args = {
         'post_report': args.post_report,
         'lat': args.lat,
-        'lon':args.lon,
+        'lon': args.lon,
         'user_id': args.user_id,
-        'user':user,
-        'text':args.text,
+        'user': user,
+        'text': args.text,
         'post_id': args.post_id,
         'report_cnt': args.report_cnt,
         'menu_id': args.menu_id,
@@ -342,25 +347,24 @@ def parse():
         'attachment_name': args.attachment_name,
         'subject': args.subject,
         'link': args.link,
-        'clazz':args.clazz,
-        'columns':args.columns,
-        'deviceid':args.deviceid,
-        'new_val':args.new_val,
+        'clazz': args.clazz,
+        'columns': args.columns,
+        'deviceid': args.deviceid,
+        'new_val': args.new_val,
     }
-    lib_map[args.example_id].__setitem__('locales',locales[args.locales])
+    lib_map[args.example_id].__setitem__('locales', locales[args.locales])
     return program_map[args.example_id], data_map[args.example_id], lib_map[args.example_id], extra_args
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     script, data_folder, lib_list, extra_args = parse()
 
     spec = spec_from_file_location("default_module", script)
     module = module_from_spec(spec)
     spec.loader.exec_module(module)
-    lib_list.__setitem__('extra_args',extra_args)
+    lib_list.__setitem__('extra_args', extra_args)
     result = analyze(module, data_folder, lib_list)
 
-    #file2 = open(r"C:\Users\sofy9\Desktop\TOP-UIC\Tesi\Results\Traccar\CPRA\get_all_columns_cpra_DEVICE.txt", "w+")
-    #file2.writelines(str(result))
+    # file2 = open(r"C:\Users\sofy9\Desktop\TOP-UIC\Tesi\Results\Traccar\CPRA\get_all_columns_cpra_DEVICE.txt", "w+")
+    # file2.writelines(str(result))
     print("\nResidual policy of the output:" + str(result))
-
