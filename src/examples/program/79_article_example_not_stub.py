07910d1c3bfd604e72e2bd09055f154c184b0319
filvan <fil.vannutelli@stud.uniroma3.it>
2025-01-17 09:19:15 -0600

    Updating ARTICLE example

--- src/examples/program/79_article_example_not_stub.py
+++ src/examples/program/79_article_example_not_stub.py
@@ -1,27 +1,27 @@
+import pandas as pd
+
+
 def run(data_folder, **kwargs):
-    # Load the libraries.
-    pd = kwargs.get('pandas')
     user_id = kwargs.get('extra_args').get('user_id')
 
     # Load the data / policies
     users = pd.read_csv(data_folder + "users/data.csv")
 
-    # users = users[users.Age >= 18]
+    users = users[users.Age >= 18]
     users = users[users.ConsentProcessing == 'Y']
-
-    """
     users = users[users.Gender == 'FEMALE']
     users = users.drop(axis=1, labels=['FirstName', 'LastName'])
-    """
-    # print("Mean age:", users.Age.mean())
-    # print("\n")
-    # print(users)
+    print("Mean age:", users.Age.mean())
+    print("\n")
+    print(users)
 
+    """
     users = users[users.NoLongerNecessary == 'Y']
     users = users[users.NecessaryForRightOfFreedomOfExpression == 'N']
     users = users[users.NecessaryForPublicInterestOrLegalObligation == 'N']
     users = users[users.NecessaryForLegalClaim == 'N']
     specific_user = users[users.DataSubjectID == user_id]
-    specific_user = specific_user.drop(axis=1, labels=['FirstName', 'LastName', 'Age', 'Gender'])
+    specific_user = specific_user.drop(axis=1, labels=['FirstName', 'LastName'])
+    """
 
-    return specific_user
+    return users
